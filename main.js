a79a3d5403bf11389b76b5b84f0af0aa0eefbb74
Zach Dennis <zach.dennis@gmail.com>
2026-07-10 10:15:12 -0400

    Bump version to v0.10.0

--- main.js
+++ main.js
@@ -405,7 +405,7 @@ ipcMain.handle('window-expand', (event, { x, y, width, height }) => {
 
 app.setAboutPanelOptions({
   applicationName: 'HTML Viewer',
-  applicationVersion: '0.9.0',
+  applicationVersion: '0.10.0',
 });
 
 // Single instance lock
